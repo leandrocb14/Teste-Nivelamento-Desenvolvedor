c798350d3721cbf4de0a542f8af73da880b9a6f7
Leandro Carvalho Batista <leandrocb14@gmail.com>
2025-03-29 23:26:34 -0300

    Respondido questões 1 ao 4. Também foi enviado a avaliação do PLSQL.

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -2,8 +2,34 @@
 
 namespace Questao1
 {
-    class ContaBancaria {
+    public class ContaBancaria
+    {
+        public ContaBancaria(int numeroConta, string titular, double saldo)
+        {
+            this.NumeroConta = numeroConta;
+            this.Titular = titular;
+            this.Saldo = saldo;
+        }
 
-       
+        public ContaBancaria(int numeroConta, string titular) : this(numeroConta, titular, 0.0) { }
+
+        public int NumeroConta { get; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public void Deposito(double quantiaDeposito)
+        {
+            Saldo += quantiaDeposito;
+        }
+
+        public void Saque(double quantiaSaque)
+        {
+            Saldo -= quantiaSaque + 3.5;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
     }
 }
--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -8,7 +8,7 @@ public class Program
         int year = 2013;
         int totalGoals = getTotalScoredGoals(teamName, year);
 
-        Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
         teamName = "Chelsea";
         year = 2014;
@@ -23,8 +23,52 @@ public class Program
 
     public static int getTotalScoredGoals(string team, int year)
     {
-        
-        return 0;
+        var goalsScoredAsHomeTeam = getTotalScoredGoals(team, year, 1, true);
+        var goalsScoredAsAwayTeam = getTotalScoredGoals(team, year, 1, false);
+
+        return goalsScoredAsHomeTeam + goalsScoredAsAwayTeam;
     }
 
+    public static int getTotalScoredGoals(string team, int year, int page, bool isHomeTeam)
+    {
+        HttpClient client = new HttpClient();
+        var url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&{(isHomeTeam ? "team1" : "team2")}={team}&page={page}";
+        var response = client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url)).Result;
+
+        if (!response.IsSuccessStatusCode)
+            throw new Exception("An error occurred while processing your request.");
+
+        var matchesSummary = JsonConvert.DeserializeObject<MatchSummary>(response.Content.ReadAsStringAsync().Result);
+        var goalsScored = 0;
+        if (matchesSummary != null && matchesSummary.data != null)
+        {
+            goalsScored += isHomeTeam ? matchesSummary.data.Sum(x => int.Parse(x.team1goals)) : matchesSummary.data.Sum(x => int.Parse(x.team2goals));
+
+            if (matchesSummary.page < matchesSummary.total_pages)
+                goalsScored += getTotalScoredGoals(team, year, matchesSummary.page + 1, isHomeTeam);
+        }
+
+        return goalsScored;
+    }
+
+}
+
+public class Match
+{
+    public string competition { get; set; }
+    public int year { get; set; }
+    public string round { get; set; }
+    public string team1 { get; set; }
+    public string team2 { get; set; }
+    public string team1goals { get; set; }
+    public string team2goals { get; set; }
+}
+
+public class MatchSummary
+{
+    public int page { get; set; }
+    public int per_page { get; set; }
+    public int total { get; set; }
+    public int total_pages { get; set; }
+    public List<Match> data { get; set; }
 }
